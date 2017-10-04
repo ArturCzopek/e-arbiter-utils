d6224790bff2192c544499554ae3f12acf634dd4
Artur Czopek <arturcz32@gmail.com>
2017-10-04 18:35:00 +0200

    #84 - added feign client for tournament results module

--- src/main/kotlin/pl/cyganki/utils/model/TaskUserDetails.kt
+++ src/main/kotlin/pl/cyganki/utils/model/TaskUserDetails.kt
@@ -1,8 +1,8 @@
 package pl.cyganki.utils.model
 
 data class TaskUserDetails(
-        val taskId: Long,
-        val pointsReceived: Float,
+        val taskId: String,
+        val earnedPoints: Long,
         val userAttempts: Int,
-        val maxAttempts: Int
+        val maxAttempts: Int?
 )
