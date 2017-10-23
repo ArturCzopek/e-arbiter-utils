262fb2c62f85f93ac2ce48da42faa429f6bb6c2b
Artur Czopek <arturcz32@gmail.com>
2017-10-23 19:34:11 +0200

    #99, #100, #102, #103 - admin panel (#11)
    - removed sysadmin role
    #102 - added admin module data class
    #102 - added annotation and controller for fetching app data
    #100 - fixed profile info

---
+++ src/main/kotlin/pl/cyganki/utils/admin/AdminServiceDataController.kt
@@ -0,0 +1,44 @@
+package pl.cyganki.utils.admin
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Controller for fetching data about service
+ * It can be used in application by using proper annotation/property.
+ *
+ * @see pl.cyganki.utils.annotation.EnableArbiterAdminServiceData
+ */
+
+@RestController
+@RequestMapping("/admin/service-data")
+@ConditionalOnProperty(value = "e-arbiter.admin.service-data.enabled", matchIfMissing = true)
+class AdminServiceDataController {
+
+    @Value("\${e-arbiter.service-name}")
+    lateinit var serviceName: String
+
+    @Value("\${server.port}")
+    lateinit var port: String
+
+    @Value("\${spring.profiles.active}")
+    lateinit var profiles: List<String>
+
+    @Value("\${logging.file}")
+    lateinit var logFilePath: String
+
+    @GetMapping(value = *arrayOf("/", ""))
+    fun getModuleData(): AdminModuleData = AdminModuleData(
+            serviceName,
+            port,
+            profiles,
+            System.getProperty("java.version"),
+            logFilePath,
+            "${Math.round(Runtime.getRuntime().freeMemory() / 10e6)} Mb",
+            "${Math.round(Runtime.getRuntime().totalMemory() / 10e6)} Mb",
+            "${Math.round(Runtime.getRuntime().maxMemory() / 10e6)} Mb"
+    )
+}
